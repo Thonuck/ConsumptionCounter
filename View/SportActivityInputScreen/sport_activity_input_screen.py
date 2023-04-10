5b740f86b4982a526c382b1d0db379d4b397b501
Thomas Nücker <thonuck@gmail.com>
2023-04-10 05:37:33 +0200

    Generalisation of input screen, SportActivity

--- View/SportActivityInputScreen/sport_activity_input_screen.py
+++ View/SportActivityInputScreen/sport_activity_input_screen.py
@@ -1,90 +1,25 @@
-from kivy.metrics import dp
-from kivy.properties import StringProperty
-from kivy.uix.anchorlayout import AnchorLayout
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.textfield import MDTextField
-from kivymd.toast import toast
-
-from View.base_app_screen import BaseAppScreenView
+from View.base_input_screen import BaseInputScreen
 
 
-class SportActivityInputScreenView(BaseAppScreenView):
+class SportActivityInputScreenView(BaseInputScreen):
     back_screen = 'sport activity screen'
     status = 'new'
 
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
 
-        layout = MDBoxLayout(orientation="vertical",
-                             spacing=5,
-                             padding=5)
+        self.elements = {'datum': {'id': 'sport_activity_datum',
+                                   'text': 'Datum'},
+                         'activity': {'id': 'sport_activity_type',
+                                      'text': 'Activity'}}
 
-        layout.add_widget(self.create_top_bar(title="Sport Activity Overview"))
+        super().__init__(input_title="Sport Activity Input",
+                         overview_screen = 'sport activity screen',
+                         **kwargs)
 
-        layout.add_widget(self.add_input_widgets())
 
-        layout.add_widget(self.create_button_frame(add=True))
-
-        self.add_widget(layout)
-
-    def add_input_widgets(self):
-        anchor_layout = AnchorLayout(anchor_y='center')
-
-        item_layout = MDBoxLayout(spacing= 5,
-                                  padding=20,
-                                  size_hint_x=0.9,
-                                  size_hint_y=0.5,
-                                  orientation='vertical')
-        
-        self.elements = {'datum': {'id': 'sport_activity_datum', 'text': 'Datum'},
-                         'activity': {'id': 'sport_activity_type', 'text': 'Activity'}}
-
-        for element_name in self.elements:
-            self.elements[element_name]['widget'] = MDTextField(hint_text=self.elements[element_name]['text'],
-                                                                # id=self.elements[element_name]['id'],
-                                                                font_size='24sp')
-            item_layout.add_widget(self.elements[element_name]['widget'])
-           
-        anchor_layout.add_widget(item_layout)
-
-        return anchor_layout
 
     def get_data(self):
         return {'datum': self.elements['datum']['widget'].text,
                 'activity': self.elements['activity']['widget'].text}
 
-    def model_is_changed(self) -> None:
-        """
-        Called whenever any change has occurrdated in the data model.
-        The view in this method tracks these changes and updates the UI
-        according to these changes.
-        """
-
-    def on_add_button_pressed(self, widget=None):
-
-        could_be_added = True
-        data = self.get_data()
-        all_filled = all([x for x in data.values()])
-        if not all_filled:
-            toast("Not able to add data")
-            return
-
-        if self.status == 'new':
-            self.controller.on_enter_data()
-        else:
-            self.controller.on_update_data()
-
-        self.manager_screens.current = 'sport activity screen'
-
-    def on_pre_enter(self, *args):
-        """Event called by screenmanager
-        fill elements depending on reason"""
-
-        self.log_info('On pre enter status {}'.format(self.status))
-
-        if self.status == 'edit':
-            self.controller.on_pre_update_data()
-        else:
-            self.elements['datum']['widget'].text = self.controller.get_date()
-            pass
---
+++ View/base_input_screen.py
@@ -0,0 +1,87 @@
+from View.base_app_screen import BaseAppScreenView
+from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivy.uix.anchorlayout import AnchorLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.textfield import MDTextField
+from kivymd.toast import toast
+
+class BaseInputScreen(BaseAppScreenView):
+    def __init__(self,
+                 input_title,
+                 overview_screen,
+                 **kwargs):
+        super().__init__(**kwargs)
+
+        self.overview_screen = overview_screen
+
+        layout = MDBoxLayout(orientation="vertical",
+                             spacing=5,
+                             padding=5)
+
+        layout.add_widget(self.create_top_bar(title=input_title))
+
+        layout.add_widget(self.add_input_widgets())
+
+        layout.add_widget(self.create_button_frame(add=True))
+
+        self.add_widget(layout)
+
+
+    def add_input_widgets(self):
+        anchor_layout = AnchorLayout(anchor_y='center')
+
+        item_layout = MDBoxLayout(spacing= 5,
+                                  padding=20,
+                                  size_hint_x=0.9,
+                                  size_hint_y=0.5,
+                                  orientation='vertical')
+        
+
+        for element_name in self.elements:
+            self.elements[element_name]['widget'] = MDTextField(hint_text=self.elements[element_name]['text'],
+                                                                # id=self.elements[element_name]['id'],
+                                                                font_size='24sp')
+            item_layout.add_widget(self.elements[element_name]['widget'])
+           
+        anchor_layout.add_widget(item_layout)
+
+        return anchor_layout
+
+
+    def model_is_changed(self) -> None:
+        """
+        Called whenever any change has occurrdated in the data model.
+        The view in this method tracks these changes and updates the UI
+        according to these changes.
+        """
+
+    def on_add_button_pressed(self, widget=None):
+
+        could_be_added = True
+        data = self.get_data()
+        all_filled = all([x for x in data.values()])
+        if not all_filled:
+            toast("Not able to add data")
+            return
+
+        if self.status == 'new':
+            self.controller.on_enter_data()
+        else:
+            self.controller.on_update_data()
+
+        self.manager_screens.current = self.back_screen 
+
+    def pre_fill_elements(self):
+        self.elements['datum']['widget'].text = self.controller.get_date()
+
+    def on_pre_enter(self, *args):
+        """Event called by screenmanager
+        fill elements depending on reason"""
+
+        self.log_info('On pre enter status {}'.format(self.status))
+
+        if self.status == 'edit':
+            self.controller.on_pre_update_data()
+        else:
+            self.pre_fill_elements()
